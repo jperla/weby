a56969a3182fbd43cb2f6d6474e64661e24846a4
Joseph Javier Perla <Notarealemail@jperla.com>
2009-12-23 08:44:18 -0600

    full request object for tornado server

--- weby/http/server/tornado_server.py
+++ weby/http/server/tornado_server.py
@@ -1,6 +1,9 @@
 from __future__ import absolute_import
 
+import sys
 import urllib
+import urlparse
+import StringIO
 
 import webob
 
@@ -14,7 +17,7 @@ def wrap_tornado(app):
     def new(tornado_request):
         #TODO: jperla: wrap into complete wsgi-like request
         args = '&'.join('%s=%s' % (urllib.quote(k),urllib.quote(v[0])) for k,v in tornado_request.arguments.iteritems())
-        req = webob.Request.blank(tornado_request.path + '?' + args)
+        req = webob_request_from_tornado_request(tornado_request)
         body = app(req)
         status, headers = body.next()
         #TODO: jperla: make this return 404 appropriately
@@ -29,6 +32,37 @@ def wrap_tornado(app):
         #TODO: jperla: make this iterate?
         tornado_request.finish()
     return new
+
+def webob_request_from_tornado_request(tornado_request):
+    #return webob.Request.blank(tornado_request.uri)
+    return webob.Request(wsgi_environ_from_tornado_request(tornado_request))
+
+
+def wsgi_environ_from_tornado_request(tornado_request):
+    '''
+    http://www.python.org/dev/peps/pep-0333/#environ-variables
+    '''
+    environ = {}
+    environ['REQUEST_METHOD'] = tornado_request.method
+    environ['SCRIPT_NAME'] = ''
+    environ['PATH_INFO'] = tornado_request.path
+    environ['QUERY_STRING'] = tornado_request.query
+    scheme, netloc, path, query, fragment = urlparse.urlsplit(tornado_request.uri)
+    server_name, server_port = netloc.split(':', 1) if (':' in netloc) else netloc, '80'
+    environ['SERVER_NAME'] = server_name
+    environ['SERVER_PORT'] = int(server_port)
+    environ['SERVER_PROTOCOL'] = tornado_request.version
+    environ['wsgi.version'] = (1, 0)
+    environ['wsgi.url_scheme'] = scheme
+    environ['wsgi.input'] = StringIO.StringIO(tornado_request.body)
+    environ['wsgi.errors'] = sys.stderr
+    environ['wsgi.multithread'] = True
+    environ['wsgi.multiprocess'] = True
+    environ['wsgi.run_once'] = False
+    for h,v in tornado_request.headers.iteritems():
+        environ['HTTP_%s' % h.upper().replace('-', '_')] = v
+    #TODO: jperla: put in content type and length for file uploads ?
+    return environ
 
 def start(app, host=None, port=8088):
     assert not isinstance(app, WSGIApp), 'Tornado server does not support WSGIApps'
